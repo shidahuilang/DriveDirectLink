fc697036231a15cf169925be1cecac96550024ea
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2021-11-06 20:13:19 +0800

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,114 @@
+<?php
+include_once('config.php');
+function send_post($url, $post_data) {
+    $postdata = http_build_query($post_data);
+    $options = array(
+        'http' => array(
+            'method' => 'POST',
+            //设置请求头
+            'header' => 'Referer: https://www.lanzous.com/\r\n'.
+                    'Accept-Language:zh-CN,zh;q=0.9\r\n',
+            'content' => $postdata,
+            'timeout' => 15 * 60 // 超时时间（单位:s）
+        )
+    );
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    return $result;
+}
+//谷歌网盘
+function google_drive($id){
+    $url = "https://drive.google.com/uc?id=" . $id . "&export=download" ;
+    global $cf_worker;
+    if(empty($cf_worker)){
+        $direct_link = "https://gd.kway.workers.dev/gd/" . $id;
+    }
+    else{
+        if($cf_worker{strlen($cf_worker)-1} == "/"){
+            $direct_link = $cf_worker . "gd/" . $id;
+        }
+        else{
+            $direct_link = $cf_worker . "/gd/" . $id;
+        }
+    }
+    header("Location: $direct_link", ture, 307);
+}
+
+//蓝奏云
+function lzy($lz){
+    $url = "https://www.lanzous.com/" . $lz;
+    $html = file_get_contents($url);
+    /*全文抓取调试
+    preg_match("/<body[^>]*?>(.*\s*?)<\/body>/is", $html, $matches);
+    */
+    preg_match_all("/<iframe[^>]*>/is", $html, $matches);  //$matches[0][1]内包含我们需要的数据
+    if(strlen($matches[0][1])>7){
+        preg_match('/src="(.*?)"/', $matches[0][1], $download_para); //正则匹配下载参数
+    }
+    else{
+        preg_match('/src="(.*?)"/', $matches[0][0], $download_para);
+    }
+    //echo $download_para[1];
+    //拼接下载链接字符串
+    $download_link = "https://www.lanzous.com/" . $download_para[1];
+    $download_html = file_get_contents($download_link);
+    //获取sign值
+    preg_match("/'sign':'(.*?)'/is", $download_html, $sign);
+    //echo $sign[1];
+    $post_data = array(
+        'action' => 'downprocess',
+        'sign' => $sign[1],
+        'ves' => '1'
+    );
+    //echo $sign[1];
+    $direct_link_json = send_post('https://www.lanzous.com/ajaxm.php', $post_data);
+    //解析json
+    $direct_link_info = json_decode($direct_link_json);
+    //$direct_link_info->dom 这个是 下载链接的拼接头
+    //$direct_link_info->url 这个是 下载链接的拼接尾
+    //拼接获取下载直链
+    $direct_link = $direct_link_info->dom . "/file/" . $direct_link_info->url;
+    if (strlen($direct_link)<40){
+        echo $direct_link;
+        return 0;
+    }
+    return $direct_link;
+    //echo '<script>window.location.href=" '.$direct_link. '";</script>';
+    //header("Location:$direct_link");
+    //echo $matches[0][0];
+}
+function link_360($id){
+    $url = "https://yunpan.360.cn/" . $id;
+    //获取header
+    file_get_contents($url);
+    //$http_response_header[5]为跳转的链接
+    $link_302=str_replace("Location: ", "", $http_response_header[5]);
+    //echo $link_302;
+    preg_match("{https://(.*?)\.}is", $link_302, $prefix_link);
+    //网址前缀
+    //echo $prefix_link[1];
+    $html = file_get_contents($link_302);
+    //获取nid
+    preg_match('/"nid": "(.*?)"/is', $html, $nid);
+    //echo $nid[1];
+
+    //设置360网盘
+    $post_data = "shorturl=" . $id . "&nid=" .$nid[1];
+    //$postdata = http_build_query($post_data);
+    $options = array(
+        'http' => array(
+            'method' => 'POST',
+            //设置请求头
+            'header' => 'Referer: ' . ".$link_302.",
+            'content' => $post_data,
+            'timeout' => 15 * 60 // 超时时间（单位:s）
+        )
+    );
+    $context = stream_context_create($options);
+    $reuqest_url ="https://" . $prefix_link[1] . ".link.yunpan.360.cn/share/downloadfile";
+    $result = file_get_contents($reuqest_url, false, $context);
+    $result_info = json_decode($result);
+    //跳转
+    return $result_info->data->downloadurl;
+}
+?>
---
+++ index.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>网盘直链下载</title>
+</head>
+<body>
+<?php
+	require_once('functions.php');
+	include_once('config.php');
+	ini_set('user_agent', 
+		'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36; 
+		zh-CN;  //蓝奏云必须设置这个且保持不变才能拿到数据
+		Nexus 4 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19');
+	//屏蔽显示
+    error_reporting(0);
+    //允许所有域访问
+	header("Access-Control-Allow-Origin: *");
+	//设置中文
+	header("Content-type:text/html;charset=utf-8");
+	//传值为谷歌网盘的处理
+	if (!empty($_GET['id'])){
+		$id = $_GET['id'];
+		google_drive($id);
+		}
+	//蓝奏云直链获取
+	elseif(!empty($_GET['lz'])){
+		$lz = $_GET['lz'];
+		$direct_link = lzy($lz);
+		if ($direct_link)
+		{
+			echo $direct_link;
+		}
+		else{
+			echo "获取直链失败，请刷新重试";
+		}
+	}
+	//蓝奏云直链下载
+	elseif(!empty($_GET['lzd'])){
+		$lz = $_GET['lzd'];
+		$direct_link = lzy($lz);
+		if ($direct_link)
+		{
+			header("Location: $direct_link"); //直接跳转到下载链接
+		}
+		else{
+			echo "获取直链失败，请刷新重试";
+		}
+	}
+	//360直链下载
+	elseif(!empty($_GET['360'])){
+		$id=$_GET['360'];
+		$direct_link = link_360($id);
+		echo $direct_link;
+		header("Location: $direct_link"); //跳转到直链下载
+	}
+	else{
+		if(empty($site_name)){
+			$site_name = "https://网站地址/";
+		}
+		echo "
+		<h1>DriveDirectLink</h1>
+
+		<p>DriveDirectLink 网盘直链下载，支持谷歌，蓝奏云</p>
+		
+		<h2>谷歌网盘直链下载</h2>
+		
+		<h3>使用方法</h3>
+		
+		<p>格式如下:
+		<br>
+		<code>
+		".$site_name."?id=文件ID
+		</code>
+		例如
+		<br>
+		google drive 分享链接
+		<br>
+		<code>
+		https://drive.google.com/open?id=1CIFH3PiEuiUJ-_6YWBfSLxi2DR9ncVmO
+		</code>
+		<br>
+		直链地址
+		<br>
+		<code>
+		".$site_name."?id=1CIFH3PiEuiUJ-_6YWBfSLxi2DR9ncVmO
+		</code></p>
+
+		<h2>蓝奏云</h2>
+		
+		<h3>获取直链地址</h3>
+		
+		<p>此方法会获取并展示直链地址，而不会直接下载
+		<br>
+		格式如下:
+		<br>
+		<code>
+		".$site_name."?lz=文件ID
+		</code>
+		<br>
+		例如
+		<br>
+		蓝奏云分享链接
+		<br>
+		<code>
+		https://www.lanzous.com/ibvifch
+		</code>
+		<br>
+		直链获取地址
+		<br>
+		<code>
+		".$site_name."?lz=ibvifch
+		</code>
+		<br>
+		注意，有时候会无法获取，刷新即可</p>
+		<h3>直链下载地址</h3>
+		<p>此方法会直接跳转下载，但是经过测试发现，国外机器使用直链下载容易验证码，酌情使用
+		<br>
+		格式如下
+		<br>
+		<code>
+		".$site_name."?lzd=文件ID
+		</code>
+		<br>
+		例如
+		<br>
+		蓝奏云分享链接
+		<br>
+		<code>
+		https://www.lanzous.com/ibvifch
+		</code>
+		<br>
+		直链下载地址
+		<br>
+		<code>
+		".$site_name."?lzd=ibvifch
+		</code></p>
+		<h2>360网盘直链下载</h2>
+		
+		<h3>使用方法</h3>
+		<p>只支持单个文件且无提取码
+		<p>格式如下:
+		<br>
+		<code>
+		".$site_name."?360=文件ID
+		</code>
+		例如
+		<br>
+		360网盘 分享链接
+		<br>
+		<code>
+		https://yunpan.360.cn/surl_yYgjWMz8GhU
+		</code>
+		<br>
+		直链地址
+		<br>
+		<code>
+		".$site_name."?360=surl_yYgjWMz8GhU
+		</code></p>
+		项目地址:<a href=\"https://github.com/Kimiato/DriveDirectLink\"> https://github.com/Kimiato/DriveDirectLink </a>
+
+		";
+	}
+?>
+</body>
